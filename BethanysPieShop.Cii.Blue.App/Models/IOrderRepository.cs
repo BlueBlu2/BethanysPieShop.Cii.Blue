99772abfa481d20145ca65f85de9e5a52fe67e22
Abdalrhmn <eng.abdalrhmn.mohamed@gmail.com>
2021-03-18 05:16:33 +0200

    08. Creating an Order Form - Finished -

---
+++ BethanysPieShop.Cii.Blue.App/Models/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Cii.Blue.App.Models
+{
+    public interface IOrderRepository
+    {
+        void CreateOrder(Order order);
+    }
+}
